862121194e775e5483803d0eeaec454f3697001f
sneha123467 <sivasnehakatteragandla@gmail.com>
2025-01-13 14:15:40 +0530

    Deployment configuration for AWS

--- src/utils.py
+++ src/utils.py
@@ -34,7 +34,7 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
             gs.fit(X_train,y_train)
 
             model.set_params(**gs.best_params_)
-            model.fit(X_train,y_train)
+            #model.fit(X_train,y_train)
 
             model.fit(X_train, y_train)  # Train model
 

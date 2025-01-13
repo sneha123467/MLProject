6bc8a043efd6f6f96144bbc083144810b5269f1c
sneha123467 <sivasnehakatteragandla@gmail.com>
2025-01-13 12:16:42 +0530

    model_trainer

--- src/utils.py
+++ src/utils.py
@@ -30,13 +30,13 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
             model = list(models.values())[i]
             para=param[list(models.keys())[i]]
 
-            gs = GridSearchCV(model,para,cv=3)
+            gs = GridSearchCV(estimator=model,param_grid=para,cv=3)
             gs.fit(X_train,y_train)
 
             model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
 
-            #model.fit(X_train, y_train)  # Train model
+            model.fit(X_train, y_train)  # Train model
 
             y_train_pred = model.predict(X_train)
 
